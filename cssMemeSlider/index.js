8a57a138be1ee2abe11c02f4702d786e643f22a3
Space Pocket <space1985pocket@gmail.com>
2023-09-14 08:09:40 +0300

    feat: add controls effects for buttons (Thu, Sep 14, 2023 08:09:40 AM)

--- cssMemeSlider/index.js
+++ cssMemeSlider/index.js
@@ -1,17 +1,24 @@
 const slides = document.querySelectorAll('.slider-images__item');
 const controlls = document.querySelectorAll('.slider-controls__item');
+const btnControlls = document.querySelectorAll('.btn-control');
 
 
+const removeActiveStatusBtn = () => {
+  btnControlls.forEach(btn => btn.classList.remove('active'));
+}
 
 
 controlls.forEach(controll => {
+
   controll.addEventListener('click', () => {
-    let position = controll.getAttribute('data-position');
+    removeActiveStatusBtn();
+
+    let position = Number(controll.getAttribute('data-position'));
+    btnControlls[position - 1].classList.add('active');
 
     slides.forEach((slide) => {
       let xPosition = slide.offsetWidth;
-
-      slide.style.transform = `translateX(${(xPosition / 10 - xPosition/10 * (Number(position)))}rem)`;
+      slide.style.transform = `translateX(${(xPosition / 10 - xPosition / 10 * (position))}rem)`;
     })
   })
 })
