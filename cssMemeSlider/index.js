1e17a7a85fb88d5d592eeba28e7dbe9557c31467
Space Pocket <space1985pocket@gmail.com>
2023-09-14 12:36:01 +0300

    feat: add animation for slide text info (Thu, Sep 14, 2023 12:36:01 PM)

--- cssMemeSlider/index.js
+++ cssMemeSlider/index.js
@@ -7,24 +7,33 @@ const slideDescription = [
   'Code Jam!?',
   'Any questions',
   'DeadLine is not coming soon',
-  'I am non tsleeping'
+  'I am non tsleeping!'
 ];
 
-sliderInfo.textContent =  slideDescription[0];
+sliderInfo.textContent = slideDescription[0];
 
-
-const removeActiveStatusBtn = () => {
-  btnControlls.forEach(btn => btn.classList.remove('active'));
-}
-
+const removeClass = (elements, elClass) => {
+  console.log('remove class', elClass);
+  for (let i = 0; i < elements.length; i += 1) {
+    elements[i].classList.remove(elClass);
+  }
+};
 
 controlls.forEach(controll => {
-
   controll.addEventListener('click', () => {
-    removeActiveStatusBtn();
+
+    removeClass(btnControlls, 'active');
+    removeClass(sliderInfo, 'titleOn');
 
     let position = Number(controll.getAttribute('data-position'));
+
+    sliderInfo.classList.add('titleOn')
     sliderInfo.textContent = slideDescription[position - 1];
+
+    setTimeout(() => {
+      console.log('set');
+      sliderInfo.classList.remove('titleOn')
+    }, 600)
 
     btnControlls[position - 1].classList.add('active');
 
@@ -34,7 +43,3 @@ controlls.forEach(controll => {
     })
   })
 })
-
-
-
-
